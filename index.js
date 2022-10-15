b28d68b49a83acf1da8e587163768236885bc4f3
avinar rezaee <avinar.rezaee@gmail.com>
2022-10-15 10:52:27 +0330

    avin

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 const fs = require('fs');
 const busboy = require('busboy');
-
+const { v4: uuidv4 } = require('uuid');
 const options = new Set();
+
 module.exports = {
     setOptions: fieldsArray => {
-        fieldsArray.filter(item => item.fieldName && item.fileName && item.filePath).forEach(item => { options.add(item) })
-        return options
+        fieldsArray.filter(item => item.fieldName && item.filePath).forEach(item => {
+            options.forEach(elem => { if (elem.fieldName == item.fieldName) options.delete(elem) });
+            options.add(item);
+        });
+        return options;
     },
     uploadFromDataUrl: async ({ dataUrl, fileName, filePath }) => {
+        const dataUrlRegex = /^data:.*\/\w+;base64,([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+        if (!dataUrlRegex.test(dataUrl)) throw new Error('dataUrl field must be DataUrl')
         const { fileTypeFromBuffer } = await import('file-type');
         const data = dataUrl.replace(/^data:.*\/\w+;base64,/, "");
         const buf = Buffer.from(data, 'base64');
         const { ext, mime } = await fileTypeFromBuffer(buf);
-        const file_name = `${fileName}.${ext}`;
+        const file_name = `${fileName ?? uuidv4()}.${ext}`;
         const file_path = `${filePath}/${file_name}`;
         try {
             fs.writeFileSync(`.${file_path}`, buf);
-            return { path: file_path };
+            return { filePath: file_path };
 
         } catch (error) {
             throw new Error(error);
         }
     },
     uploadFromBuffer: async ({ buffer, fileName, filePath }) => {
-        console.log(Buffer.isBuffer(buffer))
+        if (!Buffer.isBuffer(buffer)) throw new Error('buffer field must be Buffer')
         const { fileTypeFromBuffer } = await import('file-type');
         const { ext, mime } = await fileTypeFromBuffer(buffer);
-        const file_name = `${fileName}.${ext}`;
+        const file_name = `${fileName ?? uuidv4()}.${ext}`;
         const file_path = `${filePath}/${file_name}`;
         try {
             fs.writeFileSync(`.${file_path}`, buffer);
-            return { path: file_path };
+            return { filePath: file_path };
 
         } catch (error) {
             throw new Error(error);
@@ -40,16 +46,18 @@ module.exports = {
         const bb = busboy({ headers: req.headers });
         const files = [];
         bb.on('file', (name, file, info) => {
-            // const { filename, encoding, mimeType } = info;
+            const { filename, encoding, mimeType } = info;
             let buf = ''
             file.setEncoding('base64');
             file.on('data', (data) => {
                 buf += data;
-            }).on('close', async () => {
+            }).on('close', () => {
                 const buffer = Buffer.from(buf, 'base64');
                 const fileObj = {
                     fieldName: name,
-                    ...info,
+                    originalName: filename,
+                    encoding,
+                    mimeType,
                     buffer
                 }
                 files.push(fileObj)
@@ -73,42 +81,40 @@ module.exports = {
             file.setEncoding('base64');
             file.on('data', (data) => {
                 buf += data;
-            }).on('close', async () => {
-                try {
-                    const buffer = Buffer.from(buf, 'base64');
-                    const fileOptions = [...options].find(item => item.fieldName == name);
-                    if (fileOptions) {
-                        const { fieldName, fileName, filePath } = fileOptions
-                        // const file = await module.exports.uploadFromBuffer({ buffer, fileName, filePath });
-                        const fileObj = {
-                            fieldName,
-                            fileName,
-                            filePath,
-                            originalName: filename,
-                            encoding,
-                            mimeType,
-                            buffer
-                        }
-                        files.push(fileObj)
+            }).on('close', () => {
+                const buffer = Buffer.from(buf, 'base64');
+                const fileOptions = [...options].find(item => item.fieldName == name);
+                if (fileOptions) {
+                    const { fieldName, fileName, filePath } = fileOptions;
+                    const fileObj = {
+                        fieldName,
+                        fileName: fileName ?? uuidv4(),
+                        filePath,
+                        originalName: filename,
+                        encoding,
+                        mimeType,
+                        buffer
                     }
-                    else {
-                        const fileObj = {
-                            fieldName: name,
-                            ...info,
-                            buffer
-                        }
-                        files.push(fileObj)
-                    }
-                } catch (error) {
-                    throw new Error(error);
+                    files.push(fileObj)
                 }
             });
         });
         bb.on('field', (name, val, info) => {
             req.body[name] = val;
         });
-        bb.on('close', () => {
-            req.files = files;
+        bb.on('close', async () => {
+            const uploadedFiles = [];
+            try {
+                for (const item of files) {
+                    const { buffer, fileName, filePath } = item
+                    const file = await module.exports.uploadFromBuffer({ buffer, fileName, filePath });
+                    uploadedFiles.push(item);
+                }
+
+            } catch (error) {
+                throw new Error(error);
+            }
+            req.files = uploadedFiles;
             next();
         });
         req.pipe(bb);
